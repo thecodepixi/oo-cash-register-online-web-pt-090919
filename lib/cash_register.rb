5a6cdbc71c3503d4e918e4bd197062f8fa77b741
Emily Harber <emily.a.harber@gmail.com>
2019-10-03 23:14:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   end 
   
   def apply_discount
-    discounted_total = @total - @total/@discount 
+    discounted_total = @total * @discount 
     discounted_total
   end 
   
